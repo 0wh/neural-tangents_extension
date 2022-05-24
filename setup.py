2aa460b5bca9546b9f12bb5db276ceda8b574cf8
何偉銘 <wh249@njit.edu>
2022-05-24 02:36:14 -0400

    package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name='neural_tangents_extension',
     version='1.0',
     author='Anonymous Author',
-    package_dir={'': 'neural-tangents_extension'},
+    package_dir={'': 'neural_tangents_extension'},
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
