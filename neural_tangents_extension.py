a0cdd96abfe13b9e85b7b6a6d34ce570d0498469
何偉銘 <wh249@njit.edu>
2022-05-25 02:59:50 -0400

    package

--- neural_tangents_extension.py
+++ neural_tangents_extension.py
@@ -61,6 +61,7 @@ def _preprocess_kernel_fn_extension(kernel_fn):
       raise ValueError('invalid inputs for kernel_fn.')
     kernel = _inputs_to_kernel_extension(x1, x2, compute_ntk=compute_ntk, **reqs)
     out_kernel = kernel_fn(kernel, x=x, x_i=x_i, x_b=x_b, which=which, **kwargs)
+    print(kernel_fn.__name__)
     print(out_kernel)
     return _set_shapes(init_fn, apply_fn, kernel, out_kernel, **kwargs)
 
